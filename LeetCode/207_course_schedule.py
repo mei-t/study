31d66060c0b93496c2eb40e2582ee34883808728
mei-t <me.7tspr@gmail.com>
2022-11-26 13:30:48 +0900

    Add another solution.

--- LeetCode/207_course_schedule.py
+++ LeetCode/207_course_schedule.py
@@ -1,12 +1,12 @@
 from collections import defaultdict
-from typing import List
+from typing import List, Dict
 
 
 # N = numCourses
 # R = len(prerequisites)
 # TC: O(N + R) (while文の中では各courseに対して1回だけ処理が行われている)
 # SC: O(N + R)
-class Solution:
+class Solution1:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         firstCandi = {i for i in range(numCourses)}
         nextMap = defaultdict(set)
@@ -31,3 +31,38 @@ class Solution:
             curs = nexts
         
         return len(visited) == numCourses
+
+# N = numCourses
+# R = len(prerequisites)
+# TC: O(N + R)
+# SC: O(N + R)
+class Solution2:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        nextMap = defaultdict(list)
+        for pair in prerequisites:
+            nextCourse, prevCourse = pair[0], pair[1]
+            nextMap[prevCourse].append(nextCourse)
+        
+        visited = [False] * numCourses
+        isChecked = [False] * numCourses
+        for i in range(numCourses):
+            if self.isCycle(i, visited, isChecked, nextMap):
+                return False
+        
+        return True
+        
+    def isCycle(self, course: int, visited: List[bool], isChecked: List[bool], nextMap: Dict[int, List[int]]) -> bool:
+        if isChecked[course]:
+            return False
+        if visited[course]:
+            return True
+        
+        ret = False
+        visited[course] = True
+        for nextCourse in nextMap[course]:
+            ret = self.isCycle(nextCourse, visited, isChecked, nextMap)
+            if ret: break
+        
+        visited[course] = False
+        isChecked[course] = True
+        return ret
