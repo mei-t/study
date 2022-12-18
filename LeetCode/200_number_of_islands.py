fa53de297c9670370b6f23d1b425fb74409b4257
mei-t <me.7tspr@gmail.com>
2022-12-18 17:38:53 +0900

    Add 200_number_of_islands.py.

--- LeetCode/200_number_of_islands.py
+++ LeetCode/200_number_of_islands.py
@@ -29,3 +29,33 @@ class Solution:
         self.checkWholeIsland(r, c - 1)
         self.checkWholeIsland(r + 1, c)
         self.checkWholeIsland(r, c + 1)
+
+
+# m * n grid
+# TC: O(m * n)
+# SC: O(1))
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        self.grid = grid
+        self.row = len(self.grid)
+        self.col = len(self.grid[0])
+        res = 0
+        for i in range(self.row):
+            for j in range(self.col):
+                if self.grid[i][j] == '1':
+                    self.checkWholeIsland(i, j)
+                    res += 1
+
+        return res
+    
+    def checkWholeIsland(self, r: int, c: int) -> None:
+        if r < 0 or r >= self.row or c < 0 or c >= self.col:
+            return
+        if self.grid[r][c] == '0':
+            return
+        
+        self.grid[r][c] = '0'
+        self.checkWholeIsland(r - 1, c)
+        self.checkWholeIsland(r, c - 1)
+        self.checkWholeIsland(r + 1, c)
+        self.checkWholeIsland(r, c + 1)
